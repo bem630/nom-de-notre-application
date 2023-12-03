400114924ab30955a1d19fa0cf2987c7f6558606
bem630 <bah70495@gmail.com>
2023-12-03 18:36:43 +0100

    🔥 F01 : Delete App.test.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [ fruits, setFruits] = useState([
+    { id: 1, nom: "Mangue"},
+    { id: 2, nom: "Banane"},
+    { id: 3, nom: "Orange"}
+  ]);
+  //state
+  const [ nouveauFruit, setNouveauFruit] = useState("");
+
+  //comportements
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  }
+  const handleChange = (e) => {
+    //console.log(e.target.value);
+    setNouveauFruit(e.target.value);
+  }
+
+  //affichage (render)
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Liste de Fruits</h1>
+      <ul>{ fruits.map((fruit) => {
+        return <li key={fruit.id}>{fruit.nom}</li>
+      } ) }</ul>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input value={nouveauFruit} type="text" placeholder='Ajouter un fruit...' onChange={handleChange} />
+        <button>Ajouter</button>
+      </form>
     </div>
   );
 }
 
 export default App;
+
+
+/**!SECTION
+ * const [nouveauFruit, setNeauveauFruit] = useState("");
+  const handleDelete = (id) => {
+    const fruitsCopy = [...fruits];
+    const nouveauFruits = fruitsCopy.filter((fruit) =>  fruit.id !== id )
+    setFruits(nouveauFruits);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const fruitsCopy = [...fruits];
+    const id = new Date().getTime();
+    const nom = nouveauFruit;
+    fruitsCopy.push({id:id , nom: nom})
+    setFruits(fruitsCopy);
+  }
+  const handleChange = (event) => {
+    setNeauveauFruit(event.target.value);
+  }
+ * <ul>
+        {fruits.map((fruit) => {
+          return <li key={fruit.id}>{fruit.nom} <button onClick={() => {
+            handleDelete(fruit.id)
+          }}>x</button> </li>
+        } )}
+      </ul>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input value={nouveauFruit} type="text" onChange={handleChange} placeholder='Ajouter un Fruit...' />
+        <button>Ajouter +</button>
+      </form>
+ */
